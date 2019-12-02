4970cef935878c621df3f43ad6661ea8a6b9f53e
Andreas Meingast <ameingast@gmail.com>
2019-12-02 10:21:15 +0100

    fix: notarization-button states

--- ui/components/notarization-button.js
+++ ui/components/notarization-button.js
@@ -3,18 +3,18 @@ import {NotarizationService} from "../service/notarization.js";
 const NotarizationButtonComponent = {
   template: `
 <div>
-  <button 
-    v-if='!isNotarized() || isUnknown()' 
-  	role='button' 
-  	type='button' 
-  	class='btn btn-outline-primary' 
+  <button
+    v-if='!isNotarized() || isUnknown()'
+  	role='button'
+  	type='button'
+  	class='btn btn-outline-primary'
   	v-on:click='notarize("Notarized")'>
   	Notarize
   </button>
-  <button 
-    v-if='isNotarized() || isUnknown()' 
-  	role='button' 
-  	type='button' 
+  <button
+    v-if='isNotarized() || isUnknown()'
+  	role='button'
+  	type='button'
   	class='btn btn-outline-danger'
   	v-on:click='notarize("Untrusted")'>
   	Un-trust
@@ -28,10 +28,10 @@ const NotarizationButtonComponent = {
   },
   methods: {
     isNotarized: function () {
-      return this.image.status === 'Notarized';
+      return this.image.Notarization.Status === 'Notarized';
     },
     isUnknown: function () {
-      return !this.image.status || this.image.status === 'Unknown';
+      return !this.image.Notarization.Status || this.image.Notarization.Status === 'Unknown';
     },
     notarize: function (status) {
       const self = this;
