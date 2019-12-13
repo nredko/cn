38207d33fc3061c84e6d3a6a353485dc1969be57
Andreas Meingast <ameingast@gmail.com>
2019-12-13 17:04:06 +0100

    feat: file verification

--- pkg/cmd/verify.go
+++ pkg/cmd/verify.go
@@ -1,13 +1,17 @@
 package cmd
 
 import (
+	"errors"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	. "github.com/codenotary/ctrlt/pkg/constants"
 	"github.com/codenotary/ctrlt/pkg/container"
 	"github.com/codenotary/ctrlt/pkg/di"
+	"github.com/codenotary/ctrlt/pkg/file"
+	"github.com/codenotary/ctrlt/pkg/notary"
 	"github.com/codenotary/ctrlt/pkg/printer"
 	"github.com/codenotary/ctrlt/pkg/util"
 )
@@ -18,8 +22,7 @@ func NewVerifyCmd(output *string) *cobra.Command {
 		Aliases: []string{"v"},
 		Args:    cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			notary := di.LookupOrPanic(ContainerNotary).(container.ContainerNotary)
-			result, err := notary.GetFirstNotarizationMatchingName(args[0])
+			result, err := verify(args[0])
 			if err != nil {
 				util.Die("verification failed:", err)
 			}
@@ -27,5 +30,19 @@ func NewVerifyCmd(output *string) *cobra.Command {
 				util.Die("printing failed", err)
 			}
 		},
+	}
+}
+
+func verify(arg string) (*notary.Notarization, error) {
+	if strings.HasPrefix(arg, "docker://") {
+		n := di.LookupOrPanic(ContainerNotary).(container.ContainerNotary)
+		imageName := strings.ReplaceAll(arg, "docker://", "")
+		return n.GetFirstNotarizationMatchingName(imageName)
+	} else if strings.HasPrefix(arg, "file://") {
+		n := di.LookupOrPanic(FileNotary).(file.FileNotary)
+		path := strings.ReplaceAll(arg, "file://", "")
+		return n.Authenticate(path)
+	} else {
+		return nil, errors.New("unrecognized notarization schema")
 	}
 }
