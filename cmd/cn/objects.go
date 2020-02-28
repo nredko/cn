4b4061a78dc0cf384bb00bdc4495707e60922ebc
Nikolay Redko <redko@inexika.com>
2020-02-28 16:49:46 +0700

    feat: dir scheme

--- cmd/cn/objects.go
+++ cmd/cn/objects.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"github.com/codenotary/objects/pkg/extractor"
+	"github.com/codenotary/objects/pkg/extractor/dir"
 	"github.com/codenotary/objects/pkg/extractor/docker"
 	"github.com/codenotary/objects/pkg/extractor/file"
 	"github.com/codenotary/objects/pkg/extractor/git"
 )
 
 var _ = (func() interface{} {
+	extractor.Register(dir.Scheme, dir.Extract)
 	extractor.Register(file.Scheme, file.Extract)
 	extractor.Register(git.Scheme, git.Extract)
 	extractor.Register(docker.Scheme, docker.Extract)
 	extractor.SetFallbackScheme(file.Scheme)
+	extractor.SetFallbackScheme(dir.Scheme)
 	return nil
 })()
