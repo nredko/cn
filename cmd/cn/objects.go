232f1f07cb93f39d1fbaf6a9229a1c294d571cdd
Nikolay Redko <nredko@nredko.com>
2020-03-11 07:41:37 +0000

    feat: podman extractor

--- cmd/cn/objects.go
+++ cmd/cn/objects.go
@@ -6,10 +6,12 @@ import (
 	"github.com/codenotary/objects/pkg/extractor/docker"
 	"github.com/codenotary/objects/pkg/extractor/file"
 	"github.com/codenotary/objects/pkg/extractor/git"
+	"github.com/codenotary/objects/pkg/extractor/podman"
 	"github.com/codenotary/objects/pkg/extractor/stdin"
 )
 
 var _ = (func() interface{} {
+	extractor.Register(podman.Scheme, podman.Extract)
 	extractor.Register(stdin.Scheme, stdin.Extract)
 	extractor.Register(dir.Scheme, dir.Extract)
 	extractor.Register(file.Scheme, file.Extract)
